de204bfe5faa155d6cd79a75183cd51ffcc41440
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-11-22 00:47:21 -0500

    Make splitVarName a static method

--- src/types/var-type.js
+++ src/types/var-type.js
@@ -7,8 +7,23 @@ class DocumentedVarType extends DocumentedItem {
 
 	serialize() {
 		const names = [];
-		for(const name of this.directData.names) names.push(splitVarName(name));
+		for(const name of this.directData.names) names.push(this.constructor.splitVarName(name));
 		return { types: names };
+	}
+
+	static splitVarName(str) {
+		if(str === '*') return ['*', ''];
+		const matches = str.match(/([\w]+)([^\w]+)/g);
+		const output = [];
+		if(matches) {
+			for(const match of matches) {
+				const groups = match.match(/([\w]+)([^\w]+)/);
+				output.push([groups[1], groups[2]]);
+			}
+		} else {
+			output.push([str.match(/(\w+)/g)[0], '']);
+		}
+		return output;
 	}
 }
 
@@ -19,20 +34,5 @@ class DocumentedVarType extends DocumentedItem {
   ]
 }
 */
-
-function splitVarName(str) {
-	if(str === '*') return ['*', ''];
-	const matches = str.match(/([\w]+)([^\w]+)/g);
-	const output = [];
-	if(matches) {
-		for(const match of matches) {
-			const groups = match.match(/([\w]+)([^\w]+)/);
-			output.push([groups[1], groups[2]]);
-		}
-	} else {
-		output.push([str.match(/(\w+)/g)[0], '']);
-	}
-	return output;
-}
 
 module.exports = DocumentedVarType;
