fd3cc07a7bac329f3c6532fad1b81fb835b697bf
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-12-29 19:54:37 -0500

    Improve snazzy errors

--- src/types/item.js
+++ src/types/item.js
@@ -6,9 +6,7 @@ class DocumentedItem {
 		try {
 			this.registerMetaInfo(info);
 		} catch(err) {
-			err.message = `Error while loading ${
-				info && info.id ? `${info.id} (${this.constructor.name})` : this.constructor.name
-			}: ${err.message}`;
+			err.message = `Error while loading ${this.detailedName(info)}: ${err.message}`;
 			throw err;
 		}
 	}
@@ -17,17 +15,20 @@ class DocumentedItem {
 		try {
 			return this.serializer();
 		} catch(err) {
-			err.message = `Error while serializing ${
-				this.directData && this.directData.id ?
-				`${this.directData.id} (${this.constructor.name})` :
-				this.constructor.name
-			}: ${err.message}`;
+			err.message = `Error while serializing ${this.detailedName(this.directData)}: ${err.message}`;
 			throw err;
 		}
 	}
 
 	registerMetaInfo() { return; }
 	serializer() { return; }
+
+	detailedName(data) {
+		if(!data) return this.constructor.name;
+		if(data.id) return `${data.id} (${this.constructor.name})`;
+		if(data.name) return `${data.name} (${this.constructor.name})`;
+		return this.constructor.name;
+	}
 }
 
 module.exports = DocumentedItem;
