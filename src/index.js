11dc8952418d4f1c1cd8fe48ad7d9b6696572246
Hyro <generalkubo@gmail.com>
2021-10-22 21:13:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-const { promisify } = require('tsubaki');
 const fs = require('fs');
-const readFile = promisify(fs.readFile);
+const readFile = fs.readFileSync;
 const path = require('path');
 const jsdoc2md = require('jsdoc-to-markdown');
 const Documentation = require('./documentation');
